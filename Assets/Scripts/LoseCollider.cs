4f25438f6ebb97a2ff97b33335b72839c667ac0c
Taylor Gagne <taylor@baremetalcloud.com>
2021-06-04 02:56:35 -0400

    Added colliders and physics to sprites.
    Added colliders and physics to sprites.

--- Assets/Scripts/LoseCollider.cs
+++ Assets/Scripts/LoseCollider.cs
@@ -1,12 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class LoseCollider : MonoBehaviour
 {
-    private void OnTriggerEnter2D(Collider2D collision) 
+    // Start is called before the first frame update
+    void Start()
     {
-        SceneManager.LoadScene("Game Over");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
     }
 }
