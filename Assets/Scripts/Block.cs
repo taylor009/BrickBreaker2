231263f67a2ceebebfb221dabc8ce46a380406bf
Taylor Gagne <taylor@baremetalcloud.com>
2021-06-10 02:35:54 -0400

    Added play clip at point function to block script playing sound effect on block when it's breaking.

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 
 public class Block : MonoBehaviour
 {
+    [SerializeField] private AudioClip breakSound;
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position);
         Destroy(gameObject);
     }
 }
