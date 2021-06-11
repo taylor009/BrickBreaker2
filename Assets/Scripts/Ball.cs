e57b2b6a9afbf97b44559d26dcdf6f6f8d72142d
Taylor Gagne <taylor@baremetalcloud.com>
2021-06-11 02:34:53 -0400

    Added some randomness to the ball velocity on collision.

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,8 +1,4 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using Random = System.Random;
 
 public class Ball : MonoBehaviour
 {
@@ -11,6 +7,7 @@ public class Ball : MonoBehaviour
     [SerializeField] private float xPush = 2f;
     [SerializeField] private float yPush = 15f;
     [SerializeField] private AudioClip[] ballSounds;
+    [SerializeField] private float randomFactor = 0.2f;
 
     // state
     private Vector2 paddleToBallVector;
@@ -18,12 +15,14 @@ public class Ball : MonoBehaviour
     
     // Cached component references
     private AudioSource myAudioSource;
+    private Rigidbody2D myRigidbody2D;
 
     // Start is called before the first frame update
     void Start()
     {
         paddleToBallVector = transform.position - paddle1.transform.position;
         myAudioSource = GetComponent<AudioSource>();
+        myRigidbody2D = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -41,7 +40,7 @@ public class Ball : MonoBehaviour
         if (Input.GetMouseButtonDown(0))
         {
             hasStarted = true;
-            GetComponent<Rigidbody2D>().velocity = new Vector2(xPush, yPush);
+            myRigidbody2D.velocity = new Vector2(xPush, yPush);
         }
     }
 
@@ -53,6 +52,7 @@ public class Ball : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        Vector2 velocityTweak = new Vector2(Random.Range(0f,randomFactor),Random.Range(0f, randomFactor));
         switch (hasStarted)
         {
             case true:
@@ -60,6 +60,7 @@ public class Ball : MonoBehaviour
                 var clip = ballSounds[UnityEngine.Random.Range(0, ballSounds.Length)];
                 Debug.Log(clip);
                 myAudioSource.PlayOneShot(clip);
+                myRigidbody2D.velocity += velocityTweak;
                 break;
             }
         }
